cae5287c43abf2eda93a540a6c0abaf83a65f6ca
Gearhartlove <kristoffgearhartfinley@gmail.com>
2022-08-19 17:06:15 -0600

    Gameday Setup. Ready to branch.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
+mod mixer;
+mod camera;
+
 use bevy::prelude::*;
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub enum AppState {
+    Game
+}
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::hex("323E40").unwrap()))
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_camera)
         .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle::default());
 }
