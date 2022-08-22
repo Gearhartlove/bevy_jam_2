d8e61d607ab44facf131c3a1c5ecdd0a580c8af3
Brooks Palin <brookspalin710@gmail.com>
2022-08-21 23:23:03 -0600

    basic slots working

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod element;
 mod mixer;
 mod furnace;
 mod slicer;
+mod ui;
+mod helper;
 
 use bevy::prelude::*;
 use bevy::render::texture::ImageSettings;
@@ -10,6 +12,9 @@ use bevy_inspector_egui::{InspectorPlugin, WorldInspectorPlugin};
 use crate::mixer::MixerPlugin;
 use crate::AppState::{Game, Setup};
 use crate::registry::{MixerRecipeIden, RegistryPlugin};
+use crate::ui::UiPlugin;
+use bevy_prototype_debug_lines::DebugLinesPlugin;
+use crate::helper::{GameHelper, HelperPlugin};
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub enum AppState {
@@ -31,12 +36,17 @@ fn main() {
         .insert_resource(ClearColor(Color::hex("323E40").unwrap())) // sets background color
         .insert_resource(ImageSettings::default_nearest()) // prevents blurry sprite
         .add_plugins(DefaultPlugins)
+        .add_plugin(DebugLinesPlugin::default())
         .add_plugin(RegistryPlugin)
+        .add_plugin(HelperPlugin)
+        .add_plugin(UiPlugin)
         .add_plugin(WorldInspectorPlugin::new()) // debugging window
         .add_plugin(MixerPlugin)
         .add_startup_system(setup_camera)
         .run();
 }
+
+
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(Camera2dBundle::default());
