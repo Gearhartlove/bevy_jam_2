1c41f1389c8af4d9a1d61bee569fd4b5a58e132e
Brooks Palin <brookspalin710@gmail.com>
2022-08-27 13:19:14 -0600

    Elements and recipes in (except for mayo)

--- src/mixer.rs
+++ src/mixer.rs
@@ -3,6 +3,7 @@ use crate::MixerRecipeIden;
 use crate::registry::{Registry};
 use bevy::prelude::*;
 use crate::element::Element;
+use crate::ui::CraftType::MIXER;
 
 #[derive(PartialEq, Default, Debug, Clone)]
 pub struct MixerRecipe {
@@ -12,13 +13,30 @@ pub struct MixerRecipe {
 }
 
 impl MixerRecipe {
-    pub const TEST: MixerRecipe = MixerRecipe::new(Element::LEGEND_DAIRY, Element::MAGMA_PEPPER, Element::GLACIER_ICE);
     pub const UTTER_ICE_CREAM: MixerRecipe = MixerRecipe::new(Element::SHAVED_ICE, Element::LEGEND_DAIRY, Element::UTTER_ICE_CREAM);
+    pub const BREAD_DOUGH: MixerRecipe = MixerRecipe::new(Element::FANTASY_FLOUR, Element::YETI_WATER, Element::BREAD_DOUGH);
+    pub const ICE_CREAM_SANDWICH: MixerRecipe = MixerRecipe::new(Element::ELVEN_BREAD, Element::UTTER_ICE_CREAM, Element::ICE_CREAM_SANDWICH);
+    //pub const MAYO: MixerRecipe = MixerRecipe::new(Element::GRIFFON_EGG, Element::GRIFFON_EGG, Element::);
+    //pub const RANCH: MixerRecipe = MixerRecipe::new(Element::GRIFFON_EGG, Element::GRIFFON_EGG, Element::);
+    pub const SALAD_TOPPING: MixerRecipe = MixerRecipe::new(Element::DICED_CROUTONS, Element::RANCH, Element::SALAD_TOPPING);
+    pub const SALAD: MixerRecipe = MixerRecipe::new(Element::SIREN_SEAWEED, Element::RANCH, Element::SALAD);
+    //pub const SPICY_SPREAD: MixerRecipe = MixerRecipe::new(Element::SPICY_SPREAD, Element::, Element::UTTER_ICE_CREAM);
+    pub const SPICY_TOAST: MixerRecipe = MixerRecipe::new(Element::SPICY_SPREAD, Element::ELVEN_TOAST, Element::SPICY_TOAST);
+    pub const COOKED_PORK: MixerRecipe = MixerRecipe::new(Element::RAW_PORK, Element::BOILING_WATER, Element::COOKED_PORK);
+    pub const HARD_BOILED_EGG: MixerRecipe = MixerRecipe::new(Element::GRIFFON_EGG, Element::BOILING_WATER, Element::HARD_BOILED_EGG);
 
-    pub const RECIPES: [MixerRecipe; 2] = [
+    pub const RECIPES: [MixerRecipe; 8] = [
         MixerRecipe::UTTER_ICE_CREAM,
-        MixerRecipe::TEST,
-
+        MixerRecipe::BREAD_DOUGH,
+        MixerRecipe::ICE_CREAM_SANDWICH,
+        //
+        //
+        MixerRecipe::SALAD_TOPPING,
+        MixerRecipe::SALAD,
+        //
+        MixerRecipe::SPICY_TOAST,
+        MixerRecipe::COOKED_PORK,
+        MixerRecipe::HARD_BOILED_EGG
     ];
 
     pub const fn new(first: Element, second: Element, result: Element) -> Self {
--- src/slicer.rs
+++ src/slicer.rs
@@ -8,9 +8,19 @@ pub struct SlicerRecipe {
 
 impl SlicerRecipe {
     const SHAVED_ICE: SlicerRecipe = SlicerRecipe::new(Element::GLACIER_ICE, Element::SHAVED_ICE);
+    const PEPPER_FLAKES: SlicerRecipe = SlicerRecipe::new(Element::MAGMA_PEPPER, Element::PEPPER_FLAKES);
+    const DICED_CROUTONS: SlicerRecipe = SlicerRecipe::new(Element::ELVEN_TOAST, Element::DICED_CROUTONS);
+    const CUT_SANDWICH: SlicerRecipe = SlicerRecipe::new(Element::SANDWICH, Element::CUT_SANDWICH);
+    const BONE_CHOPSTICK: SlicerRecipe = SlicerRecipe::new(Element::BONE, Element::BONE_CHOPSTICK);
+    const RAW_BACON: SlicerRecipe = SlicerRecipe::new(Element::RAW_PORK, Element::RAW_BACON);
 
-    pub const RECIPES: [SlicerRecipe; 1] = [
+    pub const RECIPES: [SlicerRecipe; 6] = [
         SlicerRecipe::SHAVED_ICE,
+        SlicerRecipe::PEPPER_FLAKES,
+        SlicerRecipe::DICED_CROUTONS,
+        SlicerRecipe::CUT_SANDWICH,
+        SlicerRecipe::BONE_CHOPSTICK,
+        SlicerRecipe::RAW_BACON
     ];
 
     pub const fn new(object: Element, result: Element) -> Self {
